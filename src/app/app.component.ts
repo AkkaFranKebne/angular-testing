e269e3a70e58a96cd7f4ea51ad038f4f2d1ecb2f
AkkaFranKebne <ewawrobel@gmail.com>
2017-10-11 20:16:57 +0200

    components and data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']   //you can change it to "style:" and add styles here in `` 
 })
 export class AppComponent {
-
+    username = '';
+    usernameGiven  = false;
+        
+    onUpdateUsername(event: Event){
+            if (this.username.length > 0) {
+                    this.usernameGiven = true;
+                }
+                else {
+                    this.usernameGiven =  false;
+                }
+    }
+    
+    onAddingUsername(event: Event) {
+        this.username = (<HTMLInputElement>event.target).value; 
+    }
+  
 }
+
+
+
